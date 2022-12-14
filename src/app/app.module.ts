5e6f3e01f7b847adc0f495779dc54a3f9481dbd9
arath <arathramses1707@gmail.com>
2022-12-13 20:57:24 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
+import { PagesModule } from './pages/pages.module';
+import { InstructoresComponent } from './instructores/instructores.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NopageFoundComponent,
+    InstructoresComponent
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    PagesModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
